52f9d6bd54aa65edfd39ecd0f18c58e18102b29e
Viny <59251479+Viny2030@users.noreply.github.com>
2026-02-28 18:08:06 -0300

    Create orderCode.js

---
+++ utils/orderCode.js
@@ -0,0 +1,25 @@
+// utils/orderCode.js
+// Genera códigos únicos tipo VNY-2026-XXXX
+
+const fs = require('fs');
+const path = require('path');
+
+const COUNTER_FILE = path.join(__dirname, '../data/counter.json');
+
+function ensureDataDir() {
+  const dir = path.join(__dirname, '../data');
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+  if (!fs.existsSync(COUNTER_FILE)) fs.writeFileSync(COUNTER_FILE, JSON.stringify({ count: 0 }));
+}
+
+function generateOrderCode() {
+  ensureDataDir();
+  const data = JSON.parse(fs.readFileSync(COUNTER_FILE, 'utf8'));
+  data.count += 1;
+  fs.writeFileSync(COUNTER_FILE, JSON.stringify(data));
+  const year = new Date().getFullYear();
+  const num = String(data.count).padStart(4, '0');
+  return `VNY-${year}-${num}`;
+}
+
+module.exports = { generateOrderCode };
