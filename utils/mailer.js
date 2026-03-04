948898624985fd9f2ce2bde20ff39a2d345a0bec
Viny <59251479+Viny2030@users.noreply.github.com>
2026-03-04 10:22:03 -0300

    fix: cambiar from a admin@viny2030.com.ar

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, html, attachments }) {
     const { data, error } = await resend.emails.send({
-          from: 'Viny 2030 <onboarding@resend.dev>',
+                  from: 'Viny 2030 <admin@viny2030.com.ar>',
           to,
           subject,
           html,
