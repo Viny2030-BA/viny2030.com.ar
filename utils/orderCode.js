f26ebf8c9cafca31e424262c48bea95ed9ef436c
Viny <59251479+Viny2030@users.noreply.github.com>
2026-03-08 17:34:26 -0300

    Update orderCode.js

--- utils/orderCode.js
+++ utils/orderCode.js
@@ -1,22 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
-const COUNTER_FILE = path.join(__dirname, '../data/counter.json');
-
-function ensureDataDir() {
-  const dir = path.join(__dirname, '../data');
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-  if (!fs.existsSync(COUNTER_FILE)) fs.writeFileSync(COUNTER_FILE, JSON.stringify({ count: 0 }));
-}
-
 function generateOrderCode() {
-  ensureDataDir();
-  const data = JSON.parse(fs.readFileSync(COUNTER_FILE, 'utf8'));
-  data.count += 1;
-  fs.writeFileSync(COUNTER_FILE, JSON.stringify(data));
   const year = new Date().getFullYear();
-  const num = String(data.count).padStart(4, '0');
-  return `VNY-${year}-${num}`;
+  const rand = Math.random().toString(36).substring(2, 6).toUpperCase();
+  const time = Date.now().toString(36).slice(-4).toUpperCase();
+  return `VNY-${year}-${rand}${time}`;
 }
 
 module.exports = { generateOrderCode };
