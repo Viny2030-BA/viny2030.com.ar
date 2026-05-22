de93209af8d3b14f2a08ba94384a73bcc7949e1d
Viny <59251479+Viny2030@users.noreply.github.com>
2026-05-22 14:05:04 -0300

    Delete utils directory

--- utils/orderCode.js
+++
@@ -1,8 +0,0 @@
-function generateOrderCode() {
-  const year = new Date().getFullYear();
-  const rand = Math.random().toString(36).substring(2, 6).toUpperCase();
-  const time = Date.now().toString(36).slice(-4).toUpperCase();
-  return `VNY-${year}-${rand}${time}`;
-}
-
-module.exports = { generateOrderCode };
